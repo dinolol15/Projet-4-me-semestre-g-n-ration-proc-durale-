46ca7051b7ce42b732ce4d06ed03cc07871b18d2
dinolol15 <adrien.buschbeck@gmail.com>
2026-05-19 16:56:31 +0200

    change_val_WFCS

--- Projet-officiel/map_pyglet_test.py
+++ Projet-officiel/map_pyglet_test.py
@@ -22,11 +22,11 @@ def on_key_press(symbol, modifier):
         print("A was pressed")
         a = input("choix ")
         if a == "1":
-            tilemap = Carte.w_f_c_simplified(mm.create_matrix((100, 100),
+            tilemap = Carte.w_f_c_simplified(mm.create_matrix((50, 50),
                                                               {Carte.Water: 1, Carte.Coast: 2}, ))
         else:
             tilemap = Carte.w_f_c_evolved(
-                mm.create_matrix((100, 100),{"baba": 2}),
+                mm.create_matrix((50, 50),{"baba": 2}),
                 10,
                 [20, 3, Carte.Water],
                 6
@@ -40,10 +40,10 @@ def on_key_press(symbol, modifier):
                     tilemap[i][j].Color
                 except AttributeError:
                     continue
-                cell = py.shapes.Rectangle(x=50 + j*5,
-                                           y=600 + i*(-5),
-                                           width=5,
-                                           height=5,
+                cell = py.shapes.Rectangle(x=50 + j*10,
+                                           y=600 + i*(-10),
+                                           width=10,
+                                           height=10,
                                            color=tilemap[i][j].Color,
                                            batch=batch,
                                            )
